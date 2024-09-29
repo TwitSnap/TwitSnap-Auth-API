dc1c41af63bfd0c46f5baeef30a8a53a1aa2ec4d
Guido Menendez <guidomenendez0@gmail.com>
2024-09-29 20:02:35 -0300

    add logs

--- src/api/external/HttpRequester.ts
+++ src/api/external/HttpRequester.ts
@@ -1,4 +1,5 @@
 import axios, {AxiosRequestConfig, AxiosResponse} from 'axios';
+import {logger} from "../../utils/container/container";
 
 export class HttpRequester {
     public getToUrl = async <T>(
@@ -7,7 +8,17 @@ export class HttpRequester {
      catchFunction: (error: any) => void,
      extractFunction: (response: void | AxiosResponse<any, any>) => T
     ): Promise<T> => {
-        const response = await axios.get(url, params).catch(e => catchFunction(e));
+        logger.logDebugFromEntity(`Attempting HTTP GET\n 
+                                   URL: ${url}\n
+                                   Params: ${params}`
+            , this.constructor);
+
+        const response = await axios.get(url, params).catch(e => {
+            logger.logDebugFromEntity(`Attempt HTTP GET to URL ${url} has failed.`, this.constructor);
+            catchFunction(e)
+        });
+
+        logger.logDebugFromEntity(`Attempt HTTP GET to URL ${url} was successful.`, this.constructor);
         return extractFunction(response);
     }
 }
