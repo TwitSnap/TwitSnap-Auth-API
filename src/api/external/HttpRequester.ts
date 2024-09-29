dd73ea4c34f5dac08f6ca37f88fc9376528dfe2c
Guido Menendez <guidomenendez0@gmail.com>
2024-09-29 20:14:34 -0300

    add logs

--- src/api/external/HttpRequester.ts
+++ src/api/external/HttpRequester.ts
@@ -8,17 +8,39 @@ export class HttpRequester {
      catchFunction: (error: any) => void,
      extractFunction: (response: void | AxiosResponse<any, any>) => T
     ): Promise<T> => {
-        logger.logDebugFromEntity(`Attempting HTTP GET\n 
+        // ? Logea la request que se va a hacer junto con su ID unico
+        const requestId = this.getRequestUniqueId();
+        logger.logDebugFromEntity(`Attempting HTTP request...\n
+                                   ID: ${requestId}\n
+                                   Verb: GET\n
                                    URL: ${url}\n
                                    Params: ${params}`
             , this.constructor);
 
+        // ? Obtiene una response en caso de que haya ido ok, en caso de error lo maneja
         const response = await axios.get(url, params).catch(e => {
-            logger.logDebugFromEntity(`Attempt HTTP GET to URL ${url} has failed.`, this.constructor);
+            logger.logDebugFromEntity(`Attempt HTTP request
+                                        ID: ${requestId}\n
+                                        Verb: GET\n
+                                        URL: ${url}\n
+                                        Params: ${params}
+                                        Result: FAILED`
+                , this.constructor);
             catchFunction(e)
         });
 
-        logger.logDebugFromEntity(`Attempt HTTP GET to URL ${url} was successful.`, this.constructor);
+        // ? Logea resultado exitoso
+        logger.logDebugFromEntity(`Attempt HTTP request
+                                        ID: ${requestId}\n
+                                        Verb: GET\n
+                                        URL: ${url}\n
+                                        Params: ${params}
+                                        Result: SUCCESS`
+                , this.constructor);
         return extractFunction(response);
+    }
+
+    private getRequestUniqueId = (): string => {
+        return new Date().toISOString();
     }
 }
